795299b31b99f6b6aa6a078e4180b478e62146b4
Katrin Shibakova <katrina.gubanova@gmail.com>
2019-08-09 20:55:21 +0300

    added employees reducer and tests on it

---
+++ src/reducer/employees/employees.js
@@ -0,0 +1,95 @@
+const initialState = {
+  employees: [],
+  filter: ``,
+  sort: `byName`
+};
+
+const ActionType = {
+  ADD_EMPLOYEE: `ADD_EMPLOYEE`,
+  SET_EMPLOYEES: `SET_EMPLOYEES`,
+  SET_FILTER: `SET_FILTER`,
+  SET_SORT: `SET_SORT`,
+  SET_STATUS: `SET_STATUS`
+};
+
+const ActionCreator = {
+  addEmployee: (employee) => ({
+    type: ActionType.ADD_EMPLOYEE,
+    payload: employee
+  }),
+
+  setEmployees: (employeeList) => ({
+    type: ActionType.SET_EMPLOYEES,
+    payload: employeeList
+  }),
+
+  setFilter: (filter) => ({
+    type: ActionType.SET_FILTER,
+    payload: filter
+  }),
+
+  setSort: (sort) => ({
+    type: ActionType.SET_SORT,
+    payload: sort
+  }),
+
+  setStatus: (id, status) => ({
+    type: ActionType.SET_STATUS,
+    payload: {id, status}
+  })
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    case ActionType.ADD_EMPLOYEE:
+      return {
+        ...state,
+        employees: [
+          ...state.employees,
+          action.payload,
+        ]
+      };
+
+    case ActionType.SET_FILTER:
+      return {
+        ...state,
+        filter: action.payload
+      };
+
+    case ActionType.SET_SORT:
+      return {
+        ...state,
+        sort: action.payload
+      };
+
+    case ActionType.SET_EMPLOYEES:
+      return {
+        ...state,
+        employees: action.payload
+      };
+
+    case ActionType.SET_STATUS:
+      return {
+        ...state,
+        employees: state.employees.map((employee) => {
+          if (employee.id === action.payload.id) {
+            return {
+              ...employee,
+              isArchive: action.payload.status
+            };
+          }
+          return employee;
+        })
+      };
+
+    default:
+      return state;
+  }
+};
+
+export {
+  ActionCreator,
+  ActionType,
+  reducer
+};
